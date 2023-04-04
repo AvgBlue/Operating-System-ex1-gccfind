5f1c2b6a533efff0811d92bb3e4606575165b334
AvgBlue <4646dudu@gmail.com>
2023-04-04 13:45:27 +0300

    fix and adding commant to gccfind

--- gccfind.sh
+++ gccfind.sh
@@ -1,45 +1,78 @@
 #!/bin/bash
 #David Berkovits 318844685
 
-
+#check the number of parameters
 if [ $# -lt 2 ]
 then 
 	echo "Not enough parameters" 
-	exit
+	exit 0
 fi ;
 
 path=$1
 word=$2
+extension_to_delete=".out"
+extension_to_compile=".c"
 
-if [ -n "$(find $path -maxdepth 1 -name '*.out' -print -quit)" ]
+#check if files exists in the folder
+if [ -z "$(ls -A $path)" ] 
 then
-	for file_to_delete in $path/*.out; 
-	do
-		rm "$file_to_delete" 
-	done
+	exit 0
 fi
 
 
-if [ -n "$(find $path -maxdepth 1 -name '*.c' -print -quit)" ]
-then
-	file_to_compile=$(find $path -name "*.c" -exec grep -l "$word" {} \;)
-	for file in  $file_to_compile;
-	do
-		gcc -w -o ${file%.*}.out $file
-	done
-fi
-
-if [ "$3" == "-r" ]
-then
-	if [ -d "$path/*" ]
-	then 
-		exit
+#running of the for delision
+for file in $path/*
+do 
+	#check if file not exists
+	if ! [ -f "$file" ]
+	then
+		continue
 	fi
-	for folder in $path/*/
+	#check if the file extension is not equal to $extension_to_delete
+	if [[ "$(basename "$file")" != *$extension_to_delete ]]
+	then	
+		continue
+	fi
+	rm "$file" 
+done
+
+#check if files exists in the folder
+if [ -z "$(ls -A $path)" ] 
+then
+	exit 0
+fi
+
+#run on the file to compilion
+for file in $path/*
+do 
+	#check if file not exists
+	if ! [ -f "$file" ]
+	then
+		continue
+	fi
+	#check if the file extension is not equal to $extension_to_compile
+	if [[ "$(basename "$file")" != *$extension_to_compile ]]
+	then	
+		continue
+	fi
+	# check if the $word is in file
+	if grep -qw "$word"  "$file"
+	then
+		gcc -w -o ${file%.*}.out $file
+	fi
+done
+
+# checking for the not exist flag
+if [ "$3" != "-r" ]
+then
+	exit 0
+fi
+
+# run on every folder in the directory
+for folder in $path/*/
 	do
 		newpath=${folder%/}
-		$($0 $newpath $word -r) &
+		$0 $newpath $word -r &
 	done
-fi
 
 
